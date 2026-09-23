bd38077d0aa2c69570db6f0cd1e4a7fb374eec5c
Dorable <dasha.androsova@bk.ru>
2026-09-23 19:02:38 +0300

    Add backend version endpoint

--- src/main/java/self/project/web/ticket/service/controller/AuthController.java
+++ src/main/java/self/project/web/ticket/service/controller/AuthController.java
@@ -79,9 +79,4 @@ public class AuthController {
         return userRepository.findByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
     }
-
-    @GetMapping("/api/version")
-    public String version() {
-        return "v1";
-    }
 }
